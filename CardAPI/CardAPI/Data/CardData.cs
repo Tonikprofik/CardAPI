048569c16f3d4de9f0393ffbaaeefe4f824afb9e
Joe <carpenter.joseph1998@gmail.com>
2020-05-22 14:52:46 +0200

    Update CardData.cs

--- CardAPI/CardAPI/Data/CardData.cs
+++ CardAPI/CardAPI/Data/CardData.cs
@@ -18,7 +18,7 @@ namespace CardAPI.Data
                 //database context
                 var context = serviceScope.ServiceProvider.GetService<GameContext>();
 
-                //deletes all data in databse
+                //deletes all data in card table, but leaves card table itself
                 context.Database.ExecuteSqlRaw("TRUNCATE TABLE Card");
                 //re add all data to database
                 var cards = GetCards(context).ToArray();
@@ -32,7 +32,6 @@ namespace CardAPI.Data
             List<Card> cards = new List<Card>()
             {
                 //Cards related to the general 
-
                 new Card {ImageRef = "test.png", Text = "Sir!...We need more money to secure the borders, to keep the public safe from invaders", Economy1 = -15, Economy2 = 15, Military1 = 10, Military2 = -10, Relations1 = 0, Relations2 = 0, Happiness1 = 0, Happiness2 = 0, CharacterID = 1},
                 new Card {ImageRef = "test.png", Text = "Sir!.. We should implement enlistments, so we can insure that our army keeps growing", Economy1 = 0, Economy2 = 0, Military1 = 15, Military2 = -15, Relations1 = 0, Relations2 = 0, Happiness1 = -15, Happiness2 = 15, CharacterID = 1},
                 new Card {ImageRef = "test.png", Text = "Sir!.. We need to assign more soldiers to patrol the borders... We can’t trust the Foreigners", Economy1 = 0, Economy2 = 0, Military1 = -10, Military2 = 10, Relations1 = -20, Relations2 = 20, Happiness1 = 10, Happiness2 = -10, CharacterID = 1},
@@ -40,7 +39,6 @@ namespace CardAPI.Data
                 new Card {ImageRef = "test.png", Text = "Sir!.. The diplomat is a STUPID F#$@ LITTLE #$@&%* apple  #$@&%*!?!?! Don’t you agree?", Economy1 = 0, Economy2 = 0, Military1 = 10, Military2 = -10, Relations1 = -10, Relations2 = 10, Happiness1 = 0, Happiness2 = 0, CharacterID = 1},
 
                 //Cards related to the minister of trade
-
                 new Card {ImageRef = "test.png", Text = "Hey boss.. We should raise the taxes! The public don’t need that money anyway", Economy1 = 20, Economy2 = -20, Military1 = 0, Military2 = 0, Relations1 = 0, Relations2 = 0, Happiness1 = -20, Happiness2 = 20, CharacterID = 4},
                 new Card {ImageRef = "test.png", Text = "Hey boss.. We should make a trade union with our neighbouring countries!", Economy1 = 25, Economy2 = -25, Military1 = -10, Military2 = 10, Relations1 = 20, Relations2 =-20, Happiness1 = -20, Happiness2 = 20, CharacterID = 4},
                 new Card {ImageRef = "test.png", Text = "THIS IF FOR TESTING DELETE ME LATER", Economy1 = 25, Economy2 = -25, Military1 = -10, Military2 = 10, Relations1 = 20, Relations2 =-20, Happiness1 = -20, Happiness2 = 20, CharacterID = 4},
